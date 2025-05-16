99140cc64b1d303dacd750044728fc94fcf39df5
Nina Dbbs <dabboussi.nina@gmail.com>
2025-05-16 16:43:17 +0200

    build_command has been separated, redirections.c and build_cmd.c created

--- src/init_ast.c
+++ src/init_ast.c
@@ -12,13 +12,13 @@
 
 #include "../includes/minishell.h"
 
-static int	handle_single(char **args, int i, t_token *tok)
+int	handle_single(char **args, int i, t_token *tok)
 {
 	args[i++] = ft_strdup(tok->str);
 	return (i);
 }
 
-static int	add_split(char **args, int i, char *expanded)
+int	add_split(char **args, int i, char *expanded)
 {
 	char	**split;
 	int		j;
@@ -36,7 +36,7 @@ the condition strchr ", make sure if we have a string like
 $HOLA="  bonjour he ",have space at the beginning and end
 Token [wesh"$HOLA"]  quote_type 0, we won't split it, we keep "  bonjour he "
  */
-static int	handle_expanded(char **args, int i, t_token *tok, t_mini *mini)
+int	handle_expanded(char **args, int i, t_token *tok, t_mini *mini)
 {
 	char	*expanded;
 
@@ -54,7 +54,7 @@ static int	handle_expanded(char **args, int i, t_token *tok, t_mini *mini)
 	return (i);
 }
 
-static int	process_token(char **args, int i, t_token *tok, t_mini *mini)
+int	process_token(char **args, int i, t_token *tok, t_mini *mini)
 {
 	if (tok->quote_type == Q_S)
 		return (handle_single(args, i, tok));
@@ -81,7 +81,7 @@ static int	should_count_or_add(t_token *tok, t_mini *mini, char ***args, int *i)
 	return (1);
 } */
 
-static int	count_expanded_split(char *expanded)
+int	count_expanded_split(char *expanded)
 {
 	char	**split;
 	int		count = 0;
@@ -97,7 +97,7 @@ static int	count_expanded_split(char *expanded)
 	return (count);
 }
 
-static int	count_token_args(t_token *tok, t_mini *mini)
+int	count_token_args(t_token *tok, t_mini *mini)
 {
 	char	*expanded;
 	int		count = 0;
@@ -113,7 +113,7 @@ static int	count_token_args(t_token *tok, t_mini *mini)
 	return (count);
 }
 
-static int	count_args_advanced(t_token *start, t_token *end, t_mini *mini)
+int	count_args_advanced(t_token *start, t_token *end, t_mini *mini)
 {
 	int	count = 0;
 
@@ -129,7 +129,7 @@ static int	count_args_advanced(t_token *start, t_token *end, t_mini *mini)
 	return (count);
 }
 
-static char	**collect_args(t_token *start, t_token *end, t_mini *mini)
+char	**collect_args(t_token *start, t_token *end, t_mini *mini)
 {
 	int		i;
 	int		size;
@@ -153,7 +153,7 @@ static char	**collect_args(t_token *start, t_token *end, t_mini *mini)
 	return (args);
 }
 
-static char	**collect_args_for_export(t_token *start, t_token *end, t_mini *mini)
+char	**collect_args_for_export(t_token *start, t_token *end, t_mini *mini)
 {
 	int		count;
 	int		i;
@@ -194,117 +194,132 @@ static char	**collect_args_for_export(t_token *start, t_token *end, t_mini *mini
 	return (args);
 }
 
-t_cmd	*build_command(t_token *start, t_token *end, t_mini *mini)
-{
-	t_cmd	*cmd;
-	t_token	*tmp;
-	int		fd;
-	int		heredoc_nb;
-
-	heredoc_nb = 0;
-	if (start == end)
-		return (NULL);
-	tmp = start;
-	cmd = (t_cmd *)ft_calloc(1, sizeof(t_cmd));
-	if (!cmd)
-		return (NULL);
-	cmd->fd_in = -1; //to be determined;
-	cmd->fd_out = -1; //to be determined;
-	while (tmp && tmp != end)
-	{
-		if (tmp->type == R_IN && tmp->next)
-		{
-			if (cmd->infile)
-				free(cmd->infile);
-			cmd->last_redirin = 0;
-			cmd->infile = expand_arg(tmp->next->str, mini, tmp->next->quote_type);
-			fd = open(cmd->infile, O_RDONLY);
-			if (fd < 0)
-			{
-				perror(cmd->infile);
-				cmd->flag_error = 1;
-			}
-			else
-				close(fd);
-			tmp = tmp->next;
-		}
-		else if (tmp->type == R_OUT && tmp->next)
-		{
-			if (cmd->outfile)
-				free(cmd->outfile);
-			cmd->outfile = expand_arg(tmp->next->str, mini, tmp->next->quote_type);
-			if (cmd->flag_error != 1)
-			{
-				fd = open(cmd->outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-				if (fd < 0)
-				{
-					perror(cmd->outfile);
-					cmd->path_error = 1;
-				}
-				else
-					close(fd);
-				cmd->fd_out = STDOUT_FILENO;
-			}
-			tmp = tmp->next;
-		}
-		else if (tmp->type == R_A && tmp->next)
-		{
-			if (cmd->outfile)
-				free(cmd->outfile);
-			cmd->outfile = expand_arg(tmp->next->str, mini, tmp->next->quote_type);
-			if (cmd->append)
-				free(cmd->append);
-			cmd->append = expand_arg(tmp->next->str, mini, tmp->next->quote_type);
-			if (cmd->flag_error != 1)
-			{
-				fd = open(cmd->outfile, O_WRONLY | O_CREAT | O_APPEND, 0644);
-				if (fd < 0)
-				{
-					perror(cmd->outfile);
-					cmd->path_error = 1;
-				}
-				else
-					close(fd);
-				cmd->fd_out = STDOUT_FILENO;
-			}
-			tmp = tmp->next;
-		}
-		else if (tmp->type == HD && tmp->next)
-		{
-			if (cmd->infile)
-				free(cmd->infile);
-			cmd->last_redirin = 1;
-			cmd->infile = expand_arg(tmp->next->str, mini, tmp->next->quote_type);
-			heredoc_nb++;
-			tmp = tmp->next;
-		}
-		tmp = tmp->next;
-	}
-	cmd->heredoc_nb = heredoc_nb;
-	if (heredoc_nb > 0)
-	{
-		cmd->heredoc_pipe = create_heredoc_pipe(heredoc_nb);
-		cmd->heredocs = get_heredoc(heredoc_nb, start, end, cmd);
-	}
-	if (start && start->str && ft_strcmp(start->str, "export") == 0)
-		cmd->cmd_args = collect_args_for_export(start, end, mini);
-	else
-		cmd->cmd_args = collect_args(start, end, mini);
-	if (cmd->cmd_args && cmd->cmd_args[0])
-	{
-		if (cmd->cmd_args[0][0] == '/' || cmd->cmd_args[0][0] == '.')
-			cmd->cmd_path = ft_strdup(cmd->cmd_args[0]);
-		else //add and change
-		{
-			cmd->cmd_path = resolve_cmd_path(cmd->cmd_args[0], mini->env);
-			if (!cmd->cmd_path)
-				cmd->cmd_path = ft_strdup(cmd->cmd_args[0]);// <<<<<<for empty"""
-		}
-	}
-	return (cmd);
-}
-
 void	init_ast(t_mini *mini)
 {
 	mini->ast = parse_pipeline(mini->token, NULL, mini);
 }
+
+// t_cmd	*build_command(t_token *start, t_token *end, t_mini *mini)
+// {
+// 	t_cmd	*cmd;
+// 	t_token	*tmp;
+// 	int		fd;
+// 	int		heredoc_nb;
+
+// 	heredoc_nb = 0;
+// 	if (start == end)
+// 		return (NULL);
+// 	tmp = start;
+// 	cmd = (t_cmd *)ft_calloc(1, sizeof(t_cmd));
+// 	if (!cmd)
+// 		return (NULL);
+// 	cmd->fd_in = -1; //to be determined;
+// 	cmd->fd_out = -1; //to be determined;
+// 	while (tmp && tmp != end)
+// 	{
+// 		//if (tmp->type == R_IN && tmp->next)
+// 		{
+// 			//if (cmd->infile)
+// 			//	free(cmd->infile);
+// 			//cmd->last_redirin = 0;
+// 			//cmd->infile = expand_arg(tmp->next->str, mini, tmp->next->quote_type);
+// 			//fd = open(cmd->infile, O_RDONLY);
+// 			//if (fd < 0)
+// 			//{
+// 			//	perror(cmd->infile);
+// 			//	cmd->flag_error = 1;
+// 			//}
+// 			//else
+// 			//	close(fd);
+// 			//tmp = tmp->next;
+// 		}
+
+
+
+// 		//else if (tmp->type == R_OUT && tmp->next)
+// 		{
+// 		//	if (cmd->outfile)
+// 		//		free(cmd->outfile);
+// 		//	cmd->outfile = expand_arg(tmp->next->str, mini, tmp->next->quote_type);
+// 		//	if (cmd->flag_error != 1)
+// 			{
+// 			//	fd = open(cmd->outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+// 			///	if (fd < 0)
+// 			//	{
+// 			//		perror(cmd->outfile);
+// 			//		cmd->path_error = 1;
+// 			//	}
+// 			//	else
+// 			//		close(fd);
+// 			//	cmd->fd_out = STDOUT_FILENO;
+// 			//}
+// 			//tmp = tmp->next;
+// 		}
+
+
+
+
+// 		//else if (tmp->type == R_A && tmp->next)
+// 		//{
+// 		//	if (cmd->outfile)
+// 		//		free(cmd->outfile);
+// 		////	cmd->outfile = expand_arg(tmp->next->str, mini, tmp->next->quote_type);
+// 		//	if (cmd->append)
+// 		//		free(cmd->append);
+// 		//	cmd->append = expand_arg(tmp->next->str, mini, tmp->next->quote_type);
+// 		//	if (cmd->flag_error != 1)
+// 		//	{
+// 			//	fd = open(cmd->outfile, O_WRONLY | O_CREAT | O_APPEND, 0644);
+// 			//	if (fd < 0)
+// 			//	{
+// 				//	perror(cmd->outfile);
+// 			//		cmd->path_error = 1;
+// 				}
+// 			//	else
+// 			//		close(fd);
+// 			//	cmd->fd_out = STDOUT_FILENO;
+// 			}
+// 			//tmp = tmp->next;
+// 		}
+
+
+
+
+// 		//else if (tmp->type == HD && tmp->next)
+// 		{
+// 		//	if (cmd->infile)
+// 		//		free(cmd->infile);
+// 		//	cmd->last_redirin = 1;
+// 		//	cmd->infile = expand_arg(tmp->next->str, mini, tmp->next->quote_type);
+// 		//	heredoc_nb++;
+// 		//	tmp = tmp->next;
+// 		}
+// 		//tmp = tmp->next;
+// 	}
+// 	//cmd->heredoc_nb = heredoc_nb;
+// 	//if (heredoc_nb > 0)
+// 	//{
+// 	//	cmd->heredoc_pipe = create_heredoc_pipe(heredoc_nb);
+// 	//	cmd->heredocs = get_heredoc(heredoc_nb, start, end, cmd);
+// 	//}
+
+
+
+
+// 	///if (start && start->str && ft_strcmp(start->str, "export") == 0)
+// 	//	cmd->cmd_args = collect_args_for_export(start, end, mini);
+// 	//else
+// 	//	cmd->cmd_args = collect_args(start, end, mini);
+// 	//if (cmd->cmd_args && cmd->cmd_args[0])
+// 	{
+// 		//if (cmd->cmd_args[0][0] == '/' || cmd->cmd_args[0][0] == '.')
+// 		//	cmd->cmd_path = ft_strdup(cmd->cmd_args[0]);
+// 		//else //add and change
+// 		{
+// 		//	cmd->cmd_path = resolve_cmd_path(cmd->cmd_args[0], mini->env);
+// 		//	if (!cmd->cmd_path)
+// 		//		cmd->cmd_path = ft_strdup(cmd->cmd_args[0]);// <<<<<<for empty"""
+// 		}
+// 	}
+// 	//return (cmd);
+// }
