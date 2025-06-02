27501980c0816ba2b663863f57a16a9cc478d41a
Phoenixhhh <phoenixhhh@gmail.com>
2025-06-02 13:14:33 +0200

    add is a directory and no such file or directory

--- src/exec/exec_cmd_node.c
+++ src/exec/exec_cmd_node.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec_cmd_node.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ndabbous <ndabbous@student.42.fr>          #+#  +:+       +#+        */
+/*   By: hho-troc <hho-troc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025-05-29 18:40:52 by ndabbous          #+#    #+#             */
-/*   Updated: 2025-05-29 18:40:52 by ndabbous         ###   ########.fr       */
+/*   Created: 2025/05/29 18:40:52 by ndabbous          #+#    #+#             */
+/*   Updated: 2025/06/02 13:10:55 by hho-troc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,30 +20,41 @@ static void	handle_cmd_errors(t_mini *mini, t_cmd *cmd)
 		cmd->path_error = 1;
 	}
 }
-
-static void	check_cmd_path_access(t_mini *mini, t_cmd *cmd)
-{
-	if (access(cmd->cmd_path, F_OK) != 0)
-	{
-		err_msg(cmd->cmd_path, ":", "  command not found", 127);
-		safe_exit(mini, 127);
-	}
-	if (access(cmd->cmd_path, X_OK) != 0)
-	{
-		err_msg(cmd->cmd_path, ":", " Permission denied", 126);
-		safe_exit(mini, 126);
-	}
-}
+/* access can just check if the file exists and is executable
+but we need to know if it's a directory, permission denied, or not found
+so it's why I use ENOENT/EACCES*/
+// static void	check_cmd_path_access(t_mini *mini, t_cmd *cmd)
+// {
+// 	if (access(cmd->cmd_path, F_OK) != 0)
+// 	{
+// 		err_msg(cmd->cmd_path, ":", "  command not found", 127);
+// 		safe_exit(mini, 127);
+// 	}
+// 	if (access(cmd->cmd_path, X_OK) != 0)
+// 	{
+// 		err_msg(cmd->cmd_path, ":", " Permission denied", 126);
+// 		safe_exit(mini, 126);
+// 	}
+// }
 
 static void	ft_execve(t_mini *mini, t_cmd *cmd, char **envp)
 {
+	int			err;
+	struct stat	sb;
+
 	execve(cmd->cmd_path, cmd->cmd_args, envp);
-	if (access(cmd->cmd_path, X_OK) == 0)
-	{
-		perror(cmd->cmd_args[0]);
-		safe_exit(mini, 126);
-	}
-	mini->last_exit = err_msg(cmd->cmd_args[0], ":", " command not found", 127);
+	err = errno;
+	if (stat(cmd->cmd_path, &sb) == 0 && S_ISDIR(sb.st_mode))
+		mini->last_exit = err_msg(cmd->cmd_path, ":", " Is a directory", 126);
+	else if (err == EACCES)
+		mini->last_exit = err_msg(cmd->cmd_path, ":", \
+												" Permission denied", 126);
+	else if (err == ENOENT)
+		mini->last_exit = err_msg(cmd->cmd_path, ":", \
+											" No such file or directory", 127);
+	else
+		mini->last_exit = err_msg(cmd->cmd_path, ":", strerror(errno), 127);
+	safe_exit(mini, mini->last_exit);
 }
 
 void	exec_cmd_node(t_mini *mini, t_ast *node, char **envp)
@@ -66,7 +77,6 @@ void	exec_cmd_node(t_mini *mini, t_ast *node, char **envp)
 	if (cmd->cmd_args && cmd->cmd_path)
 	{
 		handle_cmd_errors(mini, cmd);
-		check_cmd_path_access(mini, cmd);
 		ft_execve(mini, cmd, envp);
 	}
 	safe_exit(mini, mini->last_exit);
